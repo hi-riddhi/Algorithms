613ece1dc2cd2e688c5af4be794f9c7844956780
riddhi <riddhisingh1101@gmail.com>
2025-10-30 13:53:52 +0530

    feat: Queue Implementation added

---
+++ DSA/QueueUsingStack.java
@@ -0,0 +1,41 @@
+import java.util.Stack;
+
+public class QueueUsingStack {
+    static class Queue {
+        Stack<Integer> s1 = new Stack<>();
+        Stack<Integer> s2 = new Stack<>();
+
+        void enqueue(int x) {
+            s1.push(x);
+        }
+
+        int dequeue() {
+            if (s2.isEmpty()) {
+                while (!s1.isEmpty()) {
+                    s2.push(s1.pop());
+                }
+            }
+            if (s2.isEmpty()) {
+                System.out.println("Queue is empty!");
+                return -1;
+            }
+            return s2.pop();
+        }
+
+        boolean isEmpty() {
+            return s1.isEmpty() && s2.isEmpty();
+        }
+    }
+
+    public static void main(String[] args) {
+        Queue q = new Queue();
+        q.enqueue(1);
+        q.enqueue(2);
+        q.enqueue(3);
+        System.out.println(q.dequeue());
+        System.out.println(q.dequeue());
+        q.enqueue(4);
+        System.out.println(q.dequeue());
+        System.out.println(q.dequeue());
+    }
+}
